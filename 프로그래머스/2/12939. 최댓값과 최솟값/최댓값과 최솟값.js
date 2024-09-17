283bea4ddd46e4aac423b739b6b4880e9b766285
Lilac00xx <154400842+SOOYEON21428@users.noreply.github.com>
2024-09-17 15:30:41 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    let numbers = s.split(' ').map(Number);
+    
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
+    
+    return `${min} ${max}`;
+}
