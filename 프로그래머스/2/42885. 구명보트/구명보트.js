19d941af65e6341a6ab70a878c9b62688e034468
Lilac00xx <joenpark06@naver.com>
2024-12-28 20:32:06 +0900

    [level 2] Title: 구명보트, Time: 32.30 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,19 +1,17 @@
 function solution(people, limit) {
     people.sort((a, b) => a - b);
 
-    let left = 0; 
-    let right = people.length - 1; 
+    let left = 0;
+    let right = people.length - 1;
     let boats = 0;
 
     while (left <= right) {
-       
         if (people[left] + people[right] <= limit) {
             left++; 
         }
-       
         right--;
         boats++;
     }
 
-    return boats; 
+    return boats;
 }
