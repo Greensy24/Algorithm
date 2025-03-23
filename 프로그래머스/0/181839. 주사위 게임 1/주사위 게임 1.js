ddb3ee0206f0f516576ddff7d8991eb016afffdc
Lilac00xx <joenpark06@naver.com>
2025-03-23 23:20:28 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    if (a % 2 === 1 && b % 2 === 1) {
+        return a * a + b * b;
+    }
+    
+    if ((a % 2 === 1 && b % 2 === 0) || (a % 2 === 0 && b % 2 === 1)) {
+        return 2 * (a + b);
+    }
+    
+    return Math.abs(a - b);
+}
